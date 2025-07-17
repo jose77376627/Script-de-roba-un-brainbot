b2f7267ed36d43289a71204d3f15889865414857
jose77376627 <evelyntlilayatzi122@gmail.com>
2025-07-17 10:52:58 -0600

    Create Nap.lua

---
+++ Nap.lua
@@ -0,0 +1,3 @@
+portada: blanca y un circulo 
+al abrir va hacer un cuadro blanco
+y en la esquina va ver x va servir para quitar el scipt al hacer eso se va poner estas seguro y abajo cuadros 2 uno ala derecha y otro ala izquierda en la derecha va ver no y en la izquierda va ver si
